6771f089ea992fb6625b878b59a867f72f4ddec5
mhyatt000 <mhyatt000@gmail.com>
2025-11-20 23:04:41 +0000

    Deploying to gh-pages from @ mhyatt000/mhyatt000.github.io@1ec251fade6880d3bc06e83f7cfb990fe8238b9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,33 +39,12 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "under construction...  The CV in the pdf is pretty up to date though!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
+        },{id: "dropdown-blog",
               title: "blog",
               description: "",
               section: "Dropdown",
